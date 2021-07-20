761e95eb1f4204b91a0cfec9a48b2ab235b2db17
Seyed Mohammad Javad Chavoshizade <mj.chavoshizade@yahoo.com>
2021-07-20 10:03:13 +0430

    feat: add getBasket and setBasket to Order model

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Collection;
 use Infrastructure\Enumerations\OrderStatusEnums;
+use Infrastructure\Enumerations\ProductStatusEnums;
 
 class OrderController extends Controller
 {
@@ -77,7 +80,42 @@ class OrderController extends Controller
      */
     public function addToBasket(Request $request)
     {
-        dd(4456);
+        $validatedData = $request->validate([
+            'quantity' => 'required|numeric',
+            'product_id' => [
+                'required',
+                Rule::exists('products', 'id')->where(function ($query) use ($request) {
+                    $query->where('status', ProductStatusEnums::AVAILABLE)
+                        ->where('inventory', '>=', $request->input('quantity'));
+                })
+            ],
+            'group_buy_product_id' => [
+                'required',
+                Rule::exists('group_buy_products', 'id')->where(function ($query) use ($request) {
+                    $query->where('end_time', '>', Carbon::now())
+                        ->where('start_time', '<=', Carbon::now())
+                        ->where('product_id', $request->input('product_id'))
+                        ->where('inventory', '>=', $request->input('quantity'))
+                        ->where('user_quantity_limit', '>=', $request->input('quantity'));
+                })
+            ]
+        ]);
+
+        $quantity = $validatedData['quantity'];
+        $product = Product::query()->findOrFail($validatedData['product_id']);
+
+        $basket = Order::getBasket(Auth::id());
+
+        $basket->orderItemsRelation()
+            ->where('product_id', $product->id);
+
+        $basket->orderItemsRelation()->create([
+            'user_id' => Auth::id(),
+            'quantity' => $quantity,
+            'amount' => $product->price,
+            'weight' => $product->weight * $quantity,
+            'product_id' => $validatedData['product_id'],
+        ]);
 
         return Auth::user();
     }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Infrastructure\Enumerations\OrderStatusEnums;
 
 class Order extends Model
 {
@@ -24,5 +26,53 @@ class Order extends Model
     public function orderItemsRelation()
     {
         return $this->hasMany(OrderItem::class);
+    }
+
+    public static function getBasket($userId, $setBasket = true): self
+    {
+        $basket = self::query()
+            ->where('user_id', $userId)
+            ->where('status', OrderStatusEnums::BASKET)
+            ->orderByDesc('id')
+            ->first();
+
+        if(!$basket && $setBasket === true) {
+            $basket = self::setBasket($userId);
+        }
+
+        return $basket;
+    }
+
+    private static function setBasket($userId): self
+    {
+        DB::beginTransaction();
+
+        try {
+            DB::unprepared('SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;');
+            DB::insert(
+                'INSERT INTO orders(user_id, amount, status, created_at)'
+                .'SELECT ?, ?, ?, ? WHERE NOT EXISTS('
+                .'SELECT 1 from orders'
+                .'WHERE user_id = ?'
+                .'AND status = ?'
+                .'AND deleted_at IS NULL '
+                .'LIMIT 1'
+                .')',
+                [
+                    $userId,
+                    0,
+                    OrderStatusEnums::BASKET,
+                    date('Y-m-d H:i:s'),
+                    $userId,
+                    OrderStatusEnums::BASKET,
+                ]
+            );
+
+            DB::commit();
+        } catch (Throwable $throwable) {
+            DB::rollback();
+        }
+
+        return self::getBasket($userId, false);
     }
 }
